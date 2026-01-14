4fc750484ed484b70fd681a32b199b9bacd44030
KyleCie <kyle.ciechowicz@gmail.com>
2026-01-14 17:27:45 +0100

    Added the new files into __init__.py

--- perfected/class_functions/__init__.py
+++ perfected/class_functions/__init__.py
@@ -1,3 +1,6 @@
-from .QueueSystem  import Queue, FastQueue  # type: ignore
-from .CommandSystem import Object, Command # type: ignore
-from .Tester       import queue_tester, fast_queue_tester, object_tester, test # type: ignore
+from .QueueSystem   import Queue, FastQueue                                     # type: ignore
+from .CommandSystem import Command                                              # type: ignore
+from .ObjectSystem  import Object                                               # type: ignore
+from .ClientSystem  import Client                                               # type: ignore
+from .FactorySystem import Factory                                              # type: ignore
+from .Tester        import queue_tester, fast_queue_tester, object_tester, test # type: ignore
